c67ed136012ce1f0fa0053be281ce39246d49a11
guohuanwen <guohuanwen@users.noreply.github.com>
2025-06-01 22:54:07 +0800

    xx

--- rcl/src/rcl/publisher.c
+++ rcl/src/rcl/publisher.c
@@ -245,10 +245,11 @@ rcl_publish(
     return RCL_RET_PUBLISHER_INVALID;  // error already set
   }
   RCL_CHECK_ARGUMENT_FOR_NULL(ros_message, RCL_RET_INVALID_ARGUMENT);
-  TRACEPOINT(rcl_publish, (const void *)publisher, (const void *)ros_message);
-  if (rmw_publish(publisher->impl->rmw_handle, ros_message, allocation) != RMW_RET_OK) {
+  TRACETOOLS_TRACEPOINT(rcl_publish, (const void *)publisher, (const void *)ros_message);
+  rcl_ret_t r = rmw_publish(publisher->impl->rmw_handle, ros_message, allocation)
+  if (r != RMW_RET_OK) {
     RCL_SET_ERROR_MSG(rmw_get_error_string().str);
-    return RCL_RET_ERROR;
+    return r;
   }
   return RCL_RET_OK;
 }
